cc70cd968b9c931c7a2226443fe62eb31efed885
Jack Biggin <me@jackbiggin.me>
2019-01-26 15:05:13 -0500

    Adding debug info

--- api/index.php
+++ api/index.php
@@ -5,11 +5,13 @@
 $action = (isset($_GET['action']) && !empty($_GET['action'])) ? $_GET['action'] : false;
 
 if(!$action) {
+    echo "No action provided";
     return false;
 }
 
 // Check if action exists
 if(!file_exists("./actions/" . $action . ".php")) {
+    echo "Action doesnt exist";
     return false;
 }
 
