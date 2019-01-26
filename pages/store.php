9aff3e20033bb5914519ce10267346eba7f67419
Evan <evan.ansell@gmail.com>
2019-01-26 18:34:29 -0500

    testing change to productbox

--- pages/store.php
+++ pages/store.php
@@ -7,12 +7,12 @@
                             <div class="product_box">
                                 <div class="row">
                                     <div class="col-12">
-                                        <img src="../assets/images/apple.svg" class="img-rounded" alt="Apple">
+                                        <img src="../assets/images/apple.svg" class="img-rounded" max-width="90%" alt="Apple">
                                     </div>
                                 </div>
                                 <div class="row">        
-                                    <div class="col-6">  
-                                         $1
+                                    <div class="col-6 text-center">  
+                                         <h3>$1</h3>
                                     </div>
                                     <div class="col-6">  
                                         <button type="button" class="btn btn-primary btn-lg" >Add to bag</button>
