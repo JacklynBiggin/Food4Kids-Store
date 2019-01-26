229687d93d88acd99cb679affb329c8d00cd1b76
Jack Biggin <me@jackbiggin.me>
2019-01-26 17:57:46 -0500

    Store layoutish

--- pages/store.php
+++ pages/store.php
@@ -1,33 +1,42 @@
 <div class="container-fluid">
         <div class="row">
             <div class="col-12 col-md-8">
-                <div class="row">
-                    <div class="col-6 col-md-3">...</div>
-                    <div class="col-6 col-md-3">...</div>
-                    <div class="col-6 col-md-3">...</div>
-                    <div class="col-6 col-md-3">...</div>
-                </div>
-                <div class="row">
-                    <div class="col-6 col-md-3">...</div>
-                    <div class="col-6 col-md-3">...</div>
-                    <div class="col-6 col-md-3">...</div>
-                    <div class="col-6 col-md-3">...</div>
-                </div>
-                <div class="row">
-                    <div class="col-6 col-md-3">...</div>
-                    <div class="col-6 col-md-3">...</div>
-                    <div class="col-6 col-md-3">...</div>
-                    <div class="col-6 col-md-3">...</div>
-                </div>
-                <div class="row">
-                    <div class="col-6 col-md-3">...</div>
-                    <div class="col-6 col-md-3">...</div>
-                    <div class="col-6 col-md-3">...</div>
-                    <div class="col-6 col-md-3">...</div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-4">
+
+                        </div>
+                    </div>
+                    <!--<div class="row">
+                        <div class="col-6 col-md-3">...</div>
+                        <div class="col-6 col-md-3">...</div>
+                        <div class="col-6 col-md-3">...</div>
+                        <div class="col-6 col-md-3">...</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6 col-md-3">...</div>
+                        <div class="col-6 col-md-3">...</div>
+                        <div class="col-6 col-md-3">...</div>
+                        <div class="col-6 col-md-3">...</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6 col-md-3">...</div>
+                        <div class="col-6 col-md-3">...</div>
+                        <div class="col-6 col-md-3">...</div>
+                        <div class="col-6 col-md-3">...</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-6 col-md-3">...</div>
+                        <div class="col-6 col-md-3">...</div>
+                        <div class="col-6 col-md-3">...</div>
+                        <div class="col-6 col-md-3">...</div>
+                    </div>-->
                 </div>
             </div>
             <aside class="col-12 col-md-4">
-                bag goes here<br>bag goes here<br>bag goes here<br>bag goes here<br>bag goes here<br>bag goes here<br>
+                <div class="container">
+                    bag goes here<br>bag goes here<br>bag goes here<br>bag goes here<br>bag goes here<br>bag goes here<br>
+                </div>
             </aside>
         </div>
 </div>
