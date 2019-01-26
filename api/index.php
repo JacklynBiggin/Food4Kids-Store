e10898b4510c681fa07d196c61cdbcbadbb12089
Jack Biggin <me@jackbiggin.me>
2019-01-26 15:14:43 -0500

    Adding json return to API

--- api/index.php
+++ api/index.php
@@ -1,18 +1,18 @@
 <?php
+require_once __DIR__ . "/requires/config.php";
+require_once __DIR__ . "/requires/functions.php";
 
 
 // Check if action is set
 $action = (isset($_GET['action']) && !empty($_GET['action'])) ? $_GET['action'] : false;
 
 if(!$action) {
-    echo "No action provided";
-    return false;
+    returnResponse(false, "No action provided");
 }
 
 // Check if action exists
 if(!file_exists("./actions/" . $action . ".php")) {
-    echo "Action doesnt exist";
-    return false;
+    returnResponse(false, "That action doesn&apos;t exist.");
 }
 
 // If it does, run action
---
+++ api/requires/config.example.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "DB_SERVER" => "",
+    "DB_DATABASE" => "",
+    "DB_USER" => "",
+    "DB_PASSWORD" => ""
+];
