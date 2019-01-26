82ea01633a5141a60579a54fee33600bd172daec
Julian Woo <julian.wooville@gmail.com>
2019-01-26 18:17:38 -0500

    Gets a single transaction

---
+++ api/actions/get_single_transaction.php
@@ -0,0 +1,16 @@
+<?php
+
+$id = (isset($_GET['id']) && !empty($_GET['id'])) ? $_GET['id'] : false;
+
+if(!$id) {
+    returnResponse(false, "No id provided");
+}
+
+ $pdo = createPDO();
+ $sql = "SELECT * FROM transactions WHERE ? limit 1";
+ $stmt = $pdo->prepare($sql);
+ $stmt->execute([$id]);
+
+ foreach($stmt as $row) {
+    returnJSON(true, [$row['id']] = ["id" => $row['id'],"email" => $row['email'],"firstname" => $row['firstname'], "lastname" => $row['lastname'], "timestamp" => $row['timestamp']]);
+}
