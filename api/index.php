246474c1c279b6e08afc8fe12f5da309a789bfcb
Julian Woo <julian.wooville@gmail.com>
2019-01-26 18:30:03 -0500

    bugfix

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
-//require_once __DIR__ . "/requires/config.php";
+require_once __DIR__ . "/requires/config.php";
 require_once __DIR__ . "/requires/functions.php";
 
 
@@ -7,12 +7,14 @@ require_once __DIR__ . "/requires/functions.php";
 $action = (isset($_GET['action']) && !empty($_GET['action'])) ? $_GET['action'] : false;
 
 if(!$action) {
-    returnResponse(false, "No action provided");
+    echo "action";
+    //returnResponse(false, "No action provided");
 }
 
 // Check if action exists
 if(!file_exists("./actions/" . $action . ".php")) {
-    returnResponse(false, "That action doesn&apos;t exist.");
+    echo "exists";
+    //    returnResponse(false, "That action doesn&apos;t exist.");
 }
 
 // If it does, run action
