fa5023bd7e0f7b77e8f4b10f79c5f35493eda8e7
Evan <evan.ansell@gmail.com>
2019-01-26 17:40:28 -0500

    Homepage usefulthings

--- pages/home.php
+++ pages/home.php
@@ -1,1 +1,6 @@
-blah blah blah
+<div class="container">
+        Main text box - explains how donation works, why user is donating, has some pretty pictures!
+   </div>
+    <div class="col text-center"> 
+      <button type="button" class="btn btn-primary btn-lg" >Next</button>
+  </div>
