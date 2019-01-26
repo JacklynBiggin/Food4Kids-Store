1a68f5432c3145711ebaecbd62fc4fe335581910
Evan <evan.ansell@gmail.com>
2019-01-26 17:45:59 -0500

    rest of files in php

---
+++ pages/store.php
@@ -0,0 +1,33 @@
+<div class="container">
+        <div class="row">
+            <div class="col-12 col-md-8">
+                <div class="row">
+                    <div class="col-6 col-md-3">...</div>
+                    <div class="col-6 col-md-3">...</div>
+                    <div class="col-6 col-md-3">...</div>
+                    <div class="col-6 col-md-3">...</div>
+                </div>
+                <div class="row">
+                    <div class="col-6 col-md-3">...</div>
+                    <div class="col-6 col-md-3">...</div>
+                    <div class="col-6 col-md-3">...</div>
+                    <div class="col-6 col-md-3">...</div>
+                </div>
+                <div class="row">
+                    <div class="col-6 col-md-3">...</div>
+                    <div class="col-6 col-md-3">...</div>
+                    <div class="col-6 col-md-3">...</div>
+                    <div class="col-6 col-md-3">...</div>
+                </div>
+                <div class="row">
+                    <div class="col-6 col-md-3">...</div>
+                    <div class="col-6 col-md-3">...</div>
+                    <div class="col-6 col-md-3">...</div>
+                    <div class="col-6 col-md-3">...</div>
+                </div>
+            </div>
+            <div class="col-12 col-md-4">
+                bag goes here<br>bag goes here<br>bag goes here<br>bag goes here<br>bag goes here<br>bag goes here<br>
+            </div>
+        </div>
+</div>
