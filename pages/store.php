0375460e4cedbc2f13e8b51eb6932542a8350b8c
Jack Biggin <me@jackbiggin.me>
2019-01-26 17:54:30 -0500

    Adding stylesheets

--- pages/store.php
+++ pages/store.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container-fluid">
         <div class="row">
             <div class="col-12 col-md-8">
                 <div class="row">
@@ -26,8 +26,8 @@
                     <div class="col-6 col-md-3">...</div>
                 </div>
             </div>
-            <div class="col-12 col-md-4">
+            <aside class="col-12 col-md-4">
                 bag goes here<br>bag goes here<br>bag goes here<br>bag goes here<br>bag goes here<br>bag goes here<br>
-            </div>
+            </aside>
         </div>
 </div>
