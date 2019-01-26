895b8050cbf6366ebb1b0d216a67f699ba182f5e
Jack Biggin <me@jackbiggin.me>
2019-01-26 17:37:01 -0500

    Load in pages based on get variable

--- index.php
+++ index.php
@@ -1,1 +1,49 @@
-Hello world!
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+    <title>Donate to Food4Kids!</title>
+  </head>
+  
+  <body>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="https://www.food4kidshamilton.ca/">
+                <img src="images/logo.png" width="248" height="120"  class="d-inline-block align-top" alt="Food4Kids"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                <a class="nav-item nav-link" href="./donation_home.html">Donate Now</a>
+                <a class="nav-item nav-link" href="https://www.food4kidshamilton.ca/about">About</a>
+                <a class="nav-item nav-link" href="https://www.food4kidshamilton.ca/programs">Programs</a>
+                <a class="nav-item nav-link" href="https://www.food4kidshamilton.ca/donate-1">Donate</a>
+                <a class="nav-item nav-link" href="https://www.food4kidshamilton.ca/get-involved">Get Involved</a>
+                </div>
+            </div>
+        </nav>
+
+        <?php
+            $page = htmlspecialchars(isset($_GET['page'])) ? $_GET['page']:'home';
+            
+            // Load in page
+            if(!file_exists("./pages/$page.php")) {
+                require_once "./pages/404.php";
+            } else {
+                require_once "./pages/$page.php";
+            }
+        ?>
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+  </body>
+</html>
