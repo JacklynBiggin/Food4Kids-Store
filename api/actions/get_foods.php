4a9917c6f3ca952369a7403489f5f97d50d88495
Jack Biggin <me@jackbiggin.me>
2019-01-26 15:26:35 -0500

    Example database call

--- api/actions/get_foods.php
+++ api/actions/get_foods.php
@@ -1,2 +1,10 @@
 <?php
-echo "test";
+
+ $pdo = createPDO();
+ $sql = "SELECT * FROM example WHERE name = ?";
+ $stmt = $pdo->prepare($sql);
+ $stmt->execute([$client_id, $client_secret]);
+ 
+ foreach($stmt as $row) {
+    echo $row['food'];
+ }
