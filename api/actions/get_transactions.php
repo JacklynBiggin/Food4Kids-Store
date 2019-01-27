6c8bcbcd8778d0e0c86bc1943480ea23f83a2dcc
Julian Woo <julian.wooville@gmail.com>
2019-01-27 07:02:01 -0500

    full names only >:(

--- api/actions/get_transactions.php
+++ api/actions/get_transactions.php
@@ -9,7 +9,7 @@
  $transinfoArray = array();
 
  foreach($stmt as $row) {
-    $transinfoArray[$row['id']] = ["id" => $row['id'],"email" => $row['email'],"firstname" => $row['firstname'], "lastname" => $row['lastname'], "timestamp" => $row['timestamp'], "customerid" => $row['customerid']];
+    $transinfoArray[$row['id']] = ["id" => $row['id'],"email" => $row['email'],"fullname" => $row['fullname'], "timestamp" => $row['timestamp'], "customerid" => $row['customerid']];
 }
 
 //Return Json
