c137f2f1dcfe5bf457d56550699a559acc4f9060
Evan <evan.ansell@gmail.com>
2019-01-27 08:33:35 -0500

    added basic links

--- pages/complete.php
+++ pages/complete.php
@@ -4,8 +4,16 @@
           Thanks to your donation of $X, X/10 kids will have food to eat for the weekend!
           Please donate again.
       </div>
-  <div class="row">
-          Insert FACEBOOK/TWITTER/?? Share-box here!
-        </div>
+  <div class="row thanks box">
+    <div class="col">
+      <a href="https://www.facebook.com/sharer/sharer.php
+      ?u=https%3A%2F%2Ffood4kids.ca%2F"
+       target="_blank">Share on Facebook</a>
+    </div>
+    <div class="col">
+      <a href="https://twitter.com/intent/tweet/
+      ?text=Check%20out%20this%20website!
+      &url=https%3A%2F%2Ffood4kids.ca%2F" 
+      target="_blank">Share on Twitter</a>
   </div>
 </div>
