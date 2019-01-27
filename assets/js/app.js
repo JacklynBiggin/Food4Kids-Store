123168926ea2aa7ec47424d4123d489c761b39d1
Evan <evan.ansell@gmail.com>
2019-01-27 03:41:33 -0500

    fixed cartlist animate

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ function addToCart(food,price,formattedprice,img_url) {
     $( "#bag-items" ).append( '<img src="' + img_url + '" class="animated fadeInDown bag-item" style="margin-left:' + margin + 'em">')
     
     // Add to cart list
-    $( "#cart-items" ).append( '<div class="cart-item"> <div class="row"> <div class="col-2"> <img src="' + img_url + '" class="animated bounceIn cart-item">  </div> <div class="col-7"> <span class="cart-item-name">' + food + '</span> </div> <div class="col-3"> <div class="cart-item-price">' + formattedprice + '</div> </div> </div> </div>')
+    $( "#cart-items" ).append( '<div class="cart-item animate bounceIn"> <div class="row"> <div class="col-2"> <img src="' + img_url + '">  </div> <div class="col-7"> <span class="cart-item-name">' + food + '</span> </div> <div class="col-3"> <div class="cart-item-price">' + formattedprice + '</div> </div> </div> </div>')
 
     // Modify cart price?
     cartPrice += parseFloat(price); 
