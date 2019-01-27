0e79116a4587dc4f14d9ab920db4d5e677422357
Julian Woo <julian.wooville@gmail.com>
2019-01-27 05:14:09 -0500

    changed some names

--- api/actions/create_charge.php
+++ api/actions/create_charge.php
@@ -4,8 +4,8 @@ require __DIR__ . '/../../vendor/autoload.php';
 $config = require_once __DIR__ . '/../requires/config.php';
 \Stripe\Stripe::setApiKey("sk_test_fJh8VXj0tMHbp4QdyVorSIXv");
 
-$token  = $_POST['stripeToken'];
-$email  = $_POST['stripeEmail'];
+$token  = $_POST['token'];
+$email  = $_POST['email'];
 $amount = $_POST['amount'];
 
 $customer = \Stripe\Customer::create([
