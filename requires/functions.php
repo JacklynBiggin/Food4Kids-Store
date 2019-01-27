16fcd2c51baae779e582fc71fbae03f881904043
Jack Biggin <me@jackbiggin.me>
2019-01-26 22:56:57 -0500

    Better API call

--- requires/functions.php
+++ requires/functions.php
@@ -1,17 +1,25 @@
 <?php
+$config = require_once __DIR__ . "/../api/requires/config.php";
 
 function getAPIdata($url){
+    global $config;
     // Get cURL resource
-$curl = curl_init();
-// Set some options - we are passing in a useragent too here
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => __DIR__ .'/../api/?'
-));
-// Send the request & save response to $resp
-$resp = curl_exec($curl);
-// Close request to clear up some resources
-curl_close($curl);
+    $curl = curl_init();
+    // Set some options - we are passing in a useragent too here
 
-return $resp;
+    $path = $config['DOMAIN_ROOT'] . '/api/?' . $url;
+
+    curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $path
+    ));
+
+    // Send the request & save response to $resp
+    $resp = curl_exec($curl);
+    // Close request to clear up some resources
+    curl_close($curl);
+
+    $resp = str_replace('"success":true,', "", $resp);
+
+    return json_decode($resp, true);
 }
