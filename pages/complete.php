26f14b7b0ae8e730fea398064b14ad355e570cfb
David <david.q778@gmail.com>
2019-01-27 08:36:30 -0500

    added twitter button

--- pages/complete.php
+++ pages/complete.php
@@ -11,9 +11,6 @@
        target="_blank">Share on Facebook</a>
     </div>
     <div class="col">
-      <a href="https://twitter.com/intent/tweet/
-      ?text=Check%20out%20this%20website!
-      &url=https%3A%2F%2Ffood4kids.ca%2F" 
-      target="_blank">Share on Twitter</a>
+    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="I just donated to Food 4 Kids Hamilton, an organization who&#39;s goal is to help feed children in the Hamilton Region! Check them out and provide a child a meal!" data-via="Food4KidsHamOnt" data-hashtags="ProvideSupport" data-lang="en" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
   </div>
 </div>
