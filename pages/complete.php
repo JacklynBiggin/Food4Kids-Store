ee984cf6329a9151922d925d1034dd694b22a69a
Evan <evan.ansell@gmail.com>
2019-01-27 08:18:08 -0500

    reverting h3.. test

--- pages/complete.php
+++ pages/complete.php
@@ -1,9 +1,8 @@
 <div class="complete-background">
   <div class="container">
       <div class="row thanks-box">
-          <h2>Thanks to your donation of $X, X/10 kids will have food to eat for the weekend!
+          Thanks to your donation of $X, X/10 kids will have food to eat for the weekend!
           Please donate again.
-          </h2>
       </div>
   <div class="row">
           Insert FACEBOOK/TWITTER/?? Share-box here!
