ff1a5fe00f374436e7e75a65680f81aeedbc00ac
Evan <evan.ansell@gmail.com>
2019-01-27 12:33:03 -0500

    trying facebook button

--- pages/complete.php
+++ pages/complete.php
@@ -6,9 +6,7 @@
       </div>
   <div class="row thanks-box">
     <div class="col">
-      <a href="https://www.facebook.com/sharer/sharer.php
-      ?u=https%3A%2F%2Ffood4kids.ca%2F"
-       target="_blank">Share on Facebook</a>
+    <div class="fb-share-button" data-href="http://www.food4kidshamilton.ca" data-layout="button" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.food4kidshamilton.ca%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
     </div>
     <div class="col">
       <a href="https://twitter.com/intent/tweet/
@@ -17,3 +15,12 @@
       target="_blank">Share on Twitter</a>
   </div>
 </div>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
