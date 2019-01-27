cd6ff97fa15b22140e6682ce29e2a5eb368155a9
Evan <evan.ansell@gmail.com>
2019-01-27 07:44:05 -0500

    trying to fit page

--- pages/complete.php
+++ pages/complete.php
@@ -1,13 +1,15 @@
-<div class="container complete-background">
-      <div class="row">
-        <p>Thanks to your donation of $X, X/10 kids will have food to eat for the weekend!
-        Please donate again.
-        </p>
-      </div>
-      <div class="row">
-        Insert FACEBOOK/TWITTER/?? Share-box here!
-      </div>
-      <div class="col text-center"> 
-          <button type="button" class="btn btn-primary btn-lg" >Next</button>
-      </div>
+<div class="complete-background">
+  <div class="container">
+        <div class="row">
+          <p>Thanks to your donation of $X, X/10 kids will have food to eat for the weekend!
+          Please donate again.
+          </p>
+        </div>
+        <div class="row">
+          Insert FACEBOOK/TWITTER/?? Share-box here!
+        </div>
+        <div class="col text-center"> 
+            <button type="button" class="btn btn-primary btn-lg" >Next</button>
+        </div>
+  </div>
 </div>
