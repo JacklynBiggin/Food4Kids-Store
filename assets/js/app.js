6ec09222a132aa10b964d6ca4736abdd04c4f00d
Evan <evan.ansell@gmail.com>
2019-01-27 04:21:01 -0500

    fixed checkout bouncin total

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,13 +11,16 @@ function addToCart(food,price,formattedprice,img_url) {
     
     // Add to cart list
     if (cartPrice == 0) {
-        $( "#cart-items" ).append(  '<a class="checkout-button" id="checkout" style="color: white;"><i class="fas fa-money-check-alt"></i> Checkout</a>');
+        $( "#cart-items" ).append(  '<a class="checkout-button animate bounceIn" id="checkout" style="color: white;"><i class="fas fa-money-check-alt"></i> Checkout</a>');
     }
     $( "#cart-items" ).append( '<div class="cart-item animate bounceIn"> <div class="row"> <div class="col-2"> <img src="' + img_url + '">  </div> <div class="col-7"> <span class="cart-item-name">' + food + '</span> </div> <div class="col-3"> <div class="cart-item-price">' + formattedprice + '</div> </div> </div> </div>');
-    $( "#checkout" ).attr("href" , "./?page=checkout&total="+cartPrice);
+    
     // Modify cart price?
     cartPrice += parseFloat(price); 
     $('#total').html("$ " + (cartPrice/100).toFixed(2));
+
+    // Modify Checkout HREF
+    $( "#checkout" ).attr("href" , "./?page=checkout&total="+cartPrice);
 }
 
 
