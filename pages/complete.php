e1f9c67f0ac654b129682d8f0ac133399e37b056
Evan <evan.ansell@gmail.com>
2019-01-27 08:14:49 -0500

    trying to fix up white background

--- pages/complete.php
+++ pages/complete.php
@@ -1,12 +1,10 @@
 <div class="complete-background">
   <div class="container">
-      <div class="row">
-        <div class="col-8-offset-2 col-xs-12 thanks-box">
-          <p>Thanks to your donation of $X, X/10 kids will have food to eat for the weekend!
+      <div class="row thanks-box">
+          <h2>Thanks to your donation of $X, X/10 kids will have food to eat for the weekend!
           Please donate again.
-          </p>
-        </div>
-  </div>
+          </h2>
+      </div>
   <div class="row">
           Insert FACEBOOK/TWITTER/?? Share-box here!
         </div>
