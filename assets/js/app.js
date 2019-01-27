ba18927e5c134d25948c9c83b1f59dcd9a9fe3bc
Evan <evan.ansell@gmail.com>
2019-01-27 01:55:39 -0500

    attempt3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 function addToCart(food,price,img_url) {
     // Create the image
-    $( "#bag-items" ).append( '<img src="' + img_url + 'animated fadeInDown">');
+    $( "#bag-items" ).append( '<img src="' + img_url + '" class="animated fadeInDown">');
     // Add to cart list
 
     // Modify cart price?
