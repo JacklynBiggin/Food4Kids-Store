4dd45e3230f88f145b91527174df2b8ce8f6d264
David <david.q778@gmail.com>
2019-01-27 10:13:45 -0500

    Added image to facebook button

--- pages/complete.php
+++ pages/complete.php
@@ -6,6 +6,17 @@
       </div>
   <div class="row thanks-box">
     <div class="col">
+
+    <!-- Js the facebook extension -->
+    <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
     <div class="fb-share-button" data-href="https://www.food4kidshamilton.ca/" data-layout="button" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.food4kidshamilton.ca%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
     </div>
     <div class="col">
