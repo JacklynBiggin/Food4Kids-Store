e14ecf2ee804e8d0a9fe87fac52ce26e18e17488
Evan <evan.ansell@gmail.com>
2019-01-27 08:07:33 -0500

    moreethinggs

--- pages/complete.php
+++ pages/complete.php
@@ -1,15 +1,14 @@
 <div class="complete-background">
   <div class="container">
-        <div class="row">
+      <div class="row">
+        <div class="col-8-offset-2 col-xs-12 thanks-box">
           <p>Thanks to your donation of $X, X/10 kids will have food to eat for the weekend!
           Please donate again.
           </p>
         </div>
-        <div class="row">
+  </div>
+  <div class="row">
           Insert FACEBOOK/TWITTER/?? Share-box here!
-        </div>
-        <div class="col text-center"> 
-            <button type="button" class="btn btn-primary btn-lg" >Next</button>
         </div>
   </div>
 </div>
