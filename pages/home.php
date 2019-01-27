fb5be76f9b5764ddaf0c3a43c27cf53d880a7d69
Evan <evan.ansell@gmail.com>
2019-01-26 19:25:12 -0500

    startshopping button

--- pages/home.php
+++ pages/home.php
@@ -2,5 +2,5 @@
         Main text box - explains how donation works, why user is donating, has some pretty pictures!
    </div>
     <div class="col text-center"> 
-      <button type="button" class="btn btn-primary btn-lg" >Next</button>
+      <a href="./?page=store" class="btn btn-primary btn-lg" ><i class="fas fa-shopping-cart"></i> Start Shopping</a>
   </div>
