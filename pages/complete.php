7540a86e426cda6194540d538a4852ac4d97a95f
David <david.q778@gmail.com>
2019-01-27 10:07:13 -0500

    added the facebook button

--- pages/complete.php
+++ pages/complete.php
@@ -6,9 +6,7 @@
       </div>
   <div class="row thanks-box">
     <div class="col">
-      <a href="https://www.facebook.com/sharer/sharer.php
-      ?u=https%3A%2F%2Ffood4kids.ca%2F"
-       target="_blank">Share on Facebook</a>
+    <div class="fb-share-button" data-href="https://www.food4kidshamilton.ca/" data-layout="button" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.food4kidshamilton.ca%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
     </div>
     <div class="col">
     <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="I just donated to Food 4 Kids Hamilton, an organization who&#39;s goal is to help feed children in the Hamilton Region! Check them out and provide a child a meal!" data-via="Food4KidsHamOnt" data-hashtags="ProvideSupport" data-lang="en" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
