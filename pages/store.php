2db7640f34bfd3d47a94be56862f139a8be09ba3
Jack Biggin <me@jackbiggin.me>
2019-01-26 22:31:16 -0500

    and the other stuff I forgot

--- pages/store.php
+++ pages/store.php
@@ -1,21 +1,26 @@
 <div class="container-fluid">
         <div class="row">
-            <div class="col-12 col-md-8">
+            <div class="col-12 col-md-8 store-background">
                 <div class="container">
                     <div class="row">
-                        <div class="col-12 col-md-4">
-                            <div class="product_box">
+                        <div class="col-12 col-lg-6 col-xl-4">
+                            <div class="product-box produce">
+                                <div class="product-header">
+                                    <h3>An Apple</h3>
+                                </div>
                                 <div class="row">
                                     <div class="col-12">
-                                        <img src="../assets/images/apple.svg" class="img-rounded" max-width="90%" alt="Apple">
+                                        <img src="./assets/images/apple.svg" class="img-rounded" max-width="90%" alt="Apple">
                                     </div>
                                 </div>
-                                <div class="row">        
-                                    <div class="col-6 text-center">  
-                                         <p><strong>$1</strong></p>
-                                    </div>
-                                    <div class="col-6">  
-                                        <button type="button" class="btn btn-primary btn-lg" >Add to bag</button>
+                                <div class="product-meta">
+                                    <div class="row">        
+                                        <div class="col-6 text-center">  
+                                            <span class="price">$1.00</span>
+                                        </div>
+                                        <div class="col-6">
+                                            <span class="add-to-cart"><i class="fas fa-cart-plus"></i> Add</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
